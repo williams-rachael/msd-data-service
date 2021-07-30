0144a8991cb229fe443a46a8ee7f7950ca133419
Rachael <williams_rachael@bah.com>
2021-07-30 13:53:49 -0400

    Generated new Spring project

---
+++ project-data-service/src/test/java/com/bah/msd/ProjectDataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bah.msd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectDataServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

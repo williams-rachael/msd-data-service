f9ee0299600a20cd9e33362067158be958f88f66
user <user@this.com>
2021-08-01 02:40:35 -0400

    my first commit

---
+++ msd-project-dataservice/src/main/java/com/bah/msd/MsdProjectDataserviceApplication.java
@@ -0,0 +1,14 @@
+package com.bah.msd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsdProjectDataserviceApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Phase 2 MSD Data Service Application Project");
+		SpringApplication.run(MsdProjectDataserviceApplication.class, args);
+	}
+
+}
---
+++ msd-project-dataservice/src/test/java/com/bah/msd/MsdProjectDataserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bah.msd;
+
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class MsdProjectDataserviceApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
